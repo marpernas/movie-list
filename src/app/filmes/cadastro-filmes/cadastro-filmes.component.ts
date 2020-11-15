58f42f9ddc45391f6598cbd5b2a62e57d5e68483
marpernas <marpernas@gmail.com>
2020-11-15 11:39:12 -0300

    first commit

---
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ValidarCamposService } from 'src/app/shared/components/campos/validar-campos.service';
+
+@Component({
+  selector: 'app-cadastro-filmes',
+  templateUrl: './cadastro-filmes.component.html',
+  styleUrls: ['./cadastro-filmes.component.scss']
+})
+export class CadastroFilmesComponent implements OnInit {
+
+  cadastro: FormGroup;
+
+  constructor(public validacao: ValidarCamposService,
+              private fb: FormBuilder) { }
+
+  get f(){
+   return this.cadastro.controls;
+  }
+
+  ngOnInit(): void {
+
+    this.cadastro = this.fb.group({
+      titulo: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['',[Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDB: ['',[Validators.minLength(10)]],
+      genero: ['', [Validators.required]]
+
+    });
+
+  }
+
+
+
+  salvar(): void{
+    if(this.cadastro.invalid){
+      return;
+    }
+
+    alert('SUCESSO!!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void{
+    this.cadastro.reset();
+  }
+
+}
